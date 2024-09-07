3f7cb7eee58a12997548d78abdebda7ec24a44ca
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-09-08 04:11:09 +0900

    style(front): Prettier added

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,35 +1,29 @@
 // This is the root layout component for your Next.js app.
 // Learn more: https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#root-layout-required
-import { cn } from '@/lib/utils';
-import { Inter } from 'next/font/google';
-import './globals.css';
+import { cn } from "@/lib/utils";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
 const fontHeading = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-heading',
-})
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-heading",
+});
 
 const fontBody = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-body',
-})
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-body",
+});
 
-export default function Layout({ children }: Readonly<{
-  children: React.ReactNode;
+export default function Layout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={cn(
-          'antialiased',
-          fontHeading.variable,
-          fontBody.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body className={cn("antialiased", fontHeading.variable, fontBody.variable)}>{children}</body>
+		</html>
+	);
 }
--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,5 +1,10 @@
 import { Slot } from "@/components/slot";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Stackr Bomb Slot",
+};
 
 export default function Home() {
-  return <Slot/>
+	return <Slot />;
 }
